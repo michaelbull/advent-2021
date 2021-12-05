73b09742c634fcce7d0934f1e23a0be82809e2c8
Michael Bull <mikebull94@gmail.com>
2021-12-05 11:59:55 +0000

    Use kotlin.test instead of junit

--- src/test/kotlin/com/github/michaelbull/advent2021/math/Vector2Test.kt
+++ src/test/kotlin/com/github/michaelbull/advent2021/math/Vector2Test.kt
@@ -1,7 +1,7 @@
 package com.github.michaelbull.advent2021.math
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Vector2Test {
 
