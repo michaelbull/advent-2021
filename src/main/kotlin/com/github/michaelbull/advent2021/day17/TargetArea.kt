6b4465a0cb0f517d2062547d61174f5dd1f99b34
Michael Bull <mikebull94@gmail.com>
2021-12-17 16:39:00 +0000

    Split up regex patterns in Day 17

--- src/main/kotlin/com/github/michaelbull/advent2021/day17/TargetArea.kt
+++ src/main/kotlin/com/github/michaelbull/advent2021/day17/TargetArea.kt
@@ -3,11 +3,13 @@ package com.github.michaelbull.advent2021.day17
 import com.github.michaelbull.advent2021.math.Vector2
 import com.github.michaelbull.advent2021.math.Vector2Range
 
-private val REGEX = "target area: x=(-?\\d+)\\.\\.(-?\\d+), y=(-?\\d+)\\.\\.(-?\\d+)".toRegex()
+private val COORDINATE_REGEX = """(-?\d+)""".toRegex()
+private val RANGE_REGEX = """$COORDINATE_REGEX\.\.$COORDINATE_REGEX""".toRegex()
+private val AREA_REGEX = "target area: x=$RANGE_REGEX, y=$RANGE_REGEX".toRegex()
 
 fun String.toTargetArea(): Vector2Range {
-    val result = requireNotNull(REGEX.matchEntire(this)) {
-        "target area must match '$REGEX', but was: $this"
+    val result = requireNotNull(AREA_REGEX.matchEntire(this)) {
+        "target area must match '$AREA_REGEX', but was: '$this'"
     }
 
     val (minX, maxX, minY, maxY) = result.groupValues.drop(1).map(String::toInt)
